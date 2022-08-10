7ed7698ed04bdf26b8caad3728c3870b6d10641e
tonghe <tonghe84@gmail.com>
2022-08-10 10:39:25 +0200

    test(topbar): added tests for topbar

---
+++ frontend-vue/src/components/TopBar/TopBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+  state: "waitlist" | "review" | "articles" | "learn" | "summary";
+  title?: string | null;
+  revcount?: number | null;
+  duecount?: number | null;
+  ahref?: string | null;
+}>();
+
+const className = ref("classy");
+</script>
+
+<template>
+  <div id="top-bar-wrapper" :class="className">
+    &nbsp;
+  </div>
+</template>
+
+<style lang="scss"></style>
